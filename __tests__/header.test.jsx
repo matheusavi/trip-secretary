521a966b2c24b8aa9a60034202f73e815a5ec200
Matheus Avi <matheusavi.47@gmail.com>
2024-06-09 20:53:02 -0300

    Add jest testing

---
+++ __tests__/header.test.jsx
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react'
+import Header from '../app/header'
+
+it('renders header unchanged', () => {
+    const { container } = render(<Header />)
+    expect(container).toMatchSnapshot()
+})
---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+};
+
+export default createJestConfig(config);
