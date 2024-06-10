0a6e41493221f336b053b659e67ec006937297d8
Matheus Avi <matheusavi.47@gmail.com>
2024-06-09 21:34:58 -0300

    Add code coverage as requirement

--- jest.config.ts
+++ jest.config.ts
@@ -16,7 +16,15 @@ const config: Config = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  }
 };
 
 export default createJestConfig(config);
