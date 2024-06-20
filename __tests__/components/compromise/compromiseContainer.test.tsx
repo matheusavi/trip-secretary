a0dbd91fc8e115a9184ca42cd1223f3be2f21c8d
Matheus Avi <matheusavi.47@gmail.com>
2024-06-19 23:27:35 -0300

    Add tests for input change

--- __tests__/components/compromise/compromiseContainer.test.tsx
+++ __tests__/components/compromise/compromiseContainer.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CompromiseContainer from "../../../app/components/compromise/compromiseContainer";
 
 describe("CompromiseContainer", () => {
@@ -42,5 +43,19 @@ describe("CompromiseContainer", () => {
     fireEvent.drop(resizable);
     expect(container).toHaveStyle("z-index: 20");
     expect(draggable).not.toHaveStyle("opacity: 0.4");
+  });
+
+  it("Elements are editable", async () => {
+    render(<CompromiseContainer id={256} />);
+    const plan = screen.getByPlaceholderText("Day plan");
+    const costs = screen.getByPlaceholderText("Costs");
+    const checkbox = screen.getByTestId("checkbox");
+    await userEvent.type(plan, " is to eat bananas");
+    await userEvent.type(costs, "10.23");
+    await userEvent.click(checkbox);
+
+    expect(plan).toHaveValue("My plan is to eat bananas");
+    expect(costs).toHaveValue("$2010.23");
+    expect(checkbox).toBeChecked();
   });
 });
